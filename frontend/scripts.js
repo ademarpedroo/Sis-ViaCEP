accc71ba56e94aa337fcff3721c1d47843c8eb65
ADEMAR\Ademar Pedro <ademarpedro2012@hotmail.com>
2022-07-05 18:46:25 -0300

    função de validar cep na api

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,31 +1,80 @@
-
 const form = document.getElementById('form');
 form.addEventListener('submit', handleSubmit);
 
+const cepValido = (cep) =>/\d{2}\.\d{3}\-\d{3}/;
+
 async function handleSubmit(event) {
   event.preventDefault();
-  
+
   const cepInputValue = document.getElementById('cep').value;
   const resultContainer = document.getElementById('resultado');
 
+  if (cepValido(cep)){
 
-  const cepResponse = await fetch('http://localhost:3000/busca_cep',{
-    method: 'POST',
-    body: JSON.stringify({
-      cep: cepInputValue
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  const responseBody = await cepResponse.json();
-  console.log(responseBody)
-
-  const layout = `
-    <span>Cep: ${cepInputValue}</span>
-    <span>Cidade: ${responseBody.localidade}</span>
-    <span>Logradouro: ${responseBody.logradouro}</span>
-  `
-
-  resultContainer.innerHTML = layout;
+      const cepResponse = await fetch('http://localhost:3000/busca_cep',{
+        method: 'POST',
+        body: JSON.stringify({
+          cep: cepInputValue
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    
+      const responseBody = await cepResponse.json();
+    
+        if(responseBody.hasOwnProperty('erro')){
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Endereço não encontrado',
+            })
+        }else {
+            const layout = `
+            <div class="container-fluid">
+              <form>
+              <fieldset disabled>
+                <div class="row">
+                  <div class="col">
+                    <label for="disabledTextInput">CEP</label>
+                    <input type="text" class="form-control" placeholder="${cepInputValue}" id="cepRetorno">
+                  </div>
+                  <div class="col">
+                    <label for="disabledTextInput">Logradouro: </label>
+                    <input type="text" class="form-control" placeholder="${responseBody.logradouro}" id="logradouro">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label for="disabledTextInput">Bairro</label>
+                    <input type="text" class="form-control" placeholder="${responseBody.bairro}" id="bairro">
+                  </div>
+                  <div class="col">
+                    <label for="disabledTextInput">Localidade</label>
+                    <input type="text" class="form-control" placeholder="${responseBody.localidade}" id="localidade">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label for="disabledTextInput">UF</label>
+                    <input type="text" class="form-control" placeholder="${responseBody.uf}" id="uf">
+                  </div>
+                  <div class="col">
+                    <label for="disabledTextInput">IBGE</label>
+                    <input type="text" class="form-control" placeholder="${responseBody.ibge}" id="ibge">
+                  </div>
+                  <div class="col">
+                    <label for="disabledTextInput">DDD</label>
+                    <input type="text" class="form-control" placeholder="${responseBody.ddd}" id="ddd">
+                  </div>
+                </div>
+              </fieldset>
+              </form>
+            </div>
+          `
+          resultContainer.innerHTML = layout;
+        }
+      }else{
+        Swal.fire('Cep invalido')  
+  }
 }
